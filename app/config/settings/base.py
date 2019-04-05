56b45b95c1470726a49b161a22f7118a472b1371
leemirim <leemirim9654@gmail.com>
2019-04-05 20:30:01 +0900

    users 앱 생성

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -39,7 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'cars.apps.CarsConfig',
+    'cars.apps.CarsConfig', # cars app
+    'users.apps.UsersConfig', # users app
+
+    'rest_framework', #django restframework
 ]
 
 MIDDLEWARE = [
