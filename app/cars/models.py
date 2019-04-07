995c597a7581407515da862feced96d6bc37bad0
leemirim <leemirim9654@gmail.com>
2019-04-08 02:16:05 +0900

    Car 상세 페이지에 필요한 property 생성 및 Car 모델에 color 필드 추가

--- app/cars/models.py
+++ app/cars/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db import models
 import datetime
+from pytz import timezone
 
 
 class TimeStampedModel(models.Model):
@@ -16,16 +17,16 @@ class Car(TimeStampedModel):
     """ Car Model """
     FUEL_CHOICES = (
         ('lpg', 'LPG'),
-        ('gasoline', 'Gasoline'),
-        ('diesel', 'Diesel'),
-        ('hybrid', 'Hybrid'),
-        ('electric', 'Electric'),
-        ('bifuel', 'Bifuel'),
+        ('휘발유', 'Gasoline'),
+        ('디젤', 'Diesel'),
+        ('하이브리드', 'Hybrid'),
+        ('전기', 'Electric'),
+        ('바이퓨얼', 'Bifuel'),
     )
 
     TRANSMISSION_CHOICES = (
-        ('auto', 'Auto'),
-        ('manual', 'Manual'),
+        ('오토', 'Auto'),
+        ('수동', 'Manual'),
     )
 
     STATUS_CHOICES = (
@@ -41,11 +42,38 @@ class Car(TimeStampedModel):
     year = models.DateField(auto_now=False, auto_now_add=False)
     fuel_type = models.CharField(max_length=80, choices=FUEL_CHOICES, null=True)
     transmission_type = models.CharField(max_length=80, choices=TRANSMISSION_CHOICES, null=True)
+    color = models.CharField(max_length=30, null=True)
     mileage = models.IntegerField()
     address = models.CharField(max_length=100)
     status = models.CharField(max_length=80, choices=STATUS_CHOICES, null=True, default='waiting')
     auction_start_time = models.DateTimeField(auto_now=False, auto_now_add=False, blank=True, null=True)
     auction_end_time = models.DateTimeField(auto_now=False, auto_now_add=False, blank=True, null=True)
+
+    @property
+    def detail_car_title(self):
+        return f'{self.brand}\n{self.kind} {self.model}'
+
+    @property
+    def detail_car_year(self):
+        return f'{self.year.strftime("%Y-%m")} ({self.year.strftime("%Y")}년형)'
+
+    @property
+    def detail_car_mileage(self):
+        return f'{self.mileage}km'
+
+    @property
+    def detail_car_info(self):
+        return f'{self.fuel_type}·{self.transmission_type}·{self.color}'
+
+    @property
+    def time_remaining(self):
+        KST = timezone('Asia/Seoul')
+        how_long = self.auction_end_time - datetime.datetime.now().replace(tzinfo=KST)
+        days, seconds = how_long.days, how_long.seconds
+        hours = days * 24 + seconds // 3600
+        mins = (seconds % 3600) // 60
+        seconds = seconds % 60
+        return f'{hours}:{mins}:{seconds}'
 
     def __str__(self):
         return f'{self.id} - {self.brand}/{self.model}'
