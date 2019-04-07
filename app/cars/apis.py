5d2b22e412c0cc62a309c25aa53ae0e2d5d70fd3
leemirim <leemirim9654@gmail.com>
2019-04-08 02:14:18 +0900

    CarAPIView 수정(aware datetime만 사용하도록)

--- app/cars/apis.py
+++ app/cars/apis.py
@@ -2,17 +2,20 @@ from rest_framework.views import APIView
 from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.response import Response
+import datetime
+from pytz import timezone
+import pytz
 
 from . import models, serializers
 from users.models import User
-import datetime
+
 
 
 class CarAPIView(APIView):
 
     def get(self, request, car_id, format=None):
         found_car = get_object_or_404(models.Car, id=car_id)
-        serializer = serializers.CarSerializer(found_car)
+        serializer = serializers.CarDetailOngingSerializer(found_car)
         return Response(data=serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request, format=None):
@@ -20,8 +23,7 @@ class CarAPIView(APIView):
         # 이후 변경 필요
         user = User.objects.get(id=1)
         car_images = request.FILES.getlist('car_images')
-        serializer = serializers.CarSerializer(data=request.data, context={
-                                                'images': car_images}, partial=True)
+        serializer = serializers.CarSerializer(data=request.data, context={'images': car_images}, partial=True)
         if serializer.is_valid():
             saved_car = serializer.save(owner=user)
             return Response(data=serializer.data, status=status.HTTP_201_CREATED)
@@ -32,11 +34,14 @@ class CarAPIView(APIView):
         found_car = get_object_or_404(models.Car, id=car_id)
         found_car.status = 'onging'
 
+        KST = pytz.timezone('Asia/Seoul')
         now = datetime.datetime.now()
+        now.replace(tzinfo=KST)
         found_car.auction_start_time = now
 
         forty_eight_hour_later = now + datetime.timedelta(hours=48)
+        forty_eight_hour_later.replace(tzinfo=KST)
         found_car.auction_end_time = forty_eight_hour_later
-        
+
         found_car.save()
         return Response(status=status.HTTP_200_OK)
