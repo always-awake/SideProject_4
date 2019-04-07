affae82ae6dc62d1f505297d3818f761b5c178a4
leemirim <leemirim9654@gmail.com>
2019-04-07 17:40:53 +0900

    Car 모델에 auction_end_time 필드 추가

--- app/cars/models.py
+++ app/cars/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+import datetime
 
 
 class TimeStampedModel(models.Model):
@@ -44,19 +45,19 @@ class Car(TimeStampedModel):
     address = models.CharField(max_length=100)
     status = models.CharField(max_length=80, choices=STATUS_CHOICES, null=True, default='waiting')
     auction_start_time = models.DateTimeField(auto_now=False, auto_now_add=False, blank=True, null=True)
+    auction_end_time = models.DateTimeField(auto_now=False, auto_now_add=False, blank=True, null=True)
 
     def __str__(self):
         return f'{self.id} - {self.brand}/{self.model}'
     
     class Meta:
-        ordering = ['-auction_start_time']
+        ordering = ['-auction_end_time']
 
     
 class Image(TimeStampedModel):
     """ Image Model """
-    image = models.ImageField(upload_to='cars', null=True)
+    image = models.ImageField(upload_to=f'cars/{datetime.datetime.today()}', null=True)
     car = models.ForeignKey(Car, on_delete=models.CASCADE, related_name='images')
-    # represent = models.BooleanField(default=False)
 
     def __str__(self):
         return f'{self.car.brand}/{self.car.model}'
