898814830929e7e76da8c546f4d767fb23898852
leemirim <leemirim9654@gmail.com>
2019-04-07 17:42:43 +0900

    CarAPIView이름 변경 및 get함수, put 함수 추가

--- app/cars/apis.py
+++ app/cars/apis.py
@@ -5,17 +5,38 @@ from rest_framework.response import Response
 
 from . import models, serializers
 from users.models import User
+import datetime
 
 
-class CarCreateAPIView(APIView):
+class CarAPIView(APIView):
+
+    def get(self, request, car_id, format=None):
+        found_car = get_object_or_404(models.Car, id=car_id)
+        serializer = serializers.CarSerializer(found_car)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
     def post(self, request, format=None):
         #user = request.user
         # 이후 변경 필요
         user = User.objects.get(id=1)
         car_images = request.FILES.getlist('car_images')
-        serializer = serializers.CarSerializer(data=request.data, context={'images': car_images}, partial=True)
+        serializer = serializers.CarSerializer(data=request.data, context={
+                                                'images': car_images}, partial=True)
         if serializer.is_valid():
             saved_car = serializer.save(owner=user)
             return Response(data=serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, car_id, format=None):
+        found_car = get_object_or_404(models.Car, id=car_id)
+        found_car.status = 'onging'
+
+        now = datetime.datetime.now()
+        found_car.auction_start_time = now
+
+        forty_eight_hour_later = now + datetime.timedelta(hours=48)
+        found_car.auction_end_time = forty_eight_hour_later
+        
+        found_car.save()
+        return Response(status=status.HTTP_200_OK)
--- app/cars/urls.py
+++ app/cars/urls.py
@@ -4,5 +4,7 @@ from . import apis
 
 app_name = 'cars'
 urlpatterns = [
-    path('new/', apis.CarCreateAPIView.as_view(), name='create_car'),
+    path('new/', apis.CarAPIView.as_view(), name='create_car'),
+    path('<int:car_id>/', apis.CarAPIView.as_view(), name='detail_car'),
+    path('<int:car_id>/approval/', apis.CarAPIView.as_view(), name='modify_car_status'),
 ]
