3df6f4c58cbd32078e14b8459371fe4af6c907fb
leemirim <leemirim9654@gmail.com>
2019-04-09 01:08:15 +0900

    api 수정에 따른 url 수정

--- app/cars/urls.py
+++ app/cars/urls.py
@@ -4,9 +4,9 @@ from . import apis
 
 app_name = 'cars'
 urlpatterns = [
-    path('', apis.CarListAPIView.as_view(), name='main_car'),
-    path('new/', apis.CarAPIView.as_view(), name='create_car'),
-    path('<int:car_id>/', apis.CarAPIView.as_view(), name='detail_car'),
-    path('<int:car_id>/approval/', apis.CarAPIView.as_view(), name='modify_car_status'),
+    path('', apis.CarListAPIView.as_view(), name='car_list'),
+    path('new/', apis.CarCreateAPIView.as_view(), name='car_new'),
+    path('<int:car_id>/', apis.CarDetailAPIView.as_view(), name='car_detail'),
+    path('<int:car_id>/approval/', apis.CarCreateAPIView.as_view(), name='car_modify_status'),
 
 ]
