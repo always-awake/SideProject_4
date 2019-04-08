d5e90d63af1a984dd5ccffca3e98f5a56e32a0b8
leemirim <leemirim9654@gmail.com>
2019-04-08 15:02:16 +0900

    CarList(메인페이지) url 추가

--- app/cars/urls.py
+++ app/cars/urls.py
@@ -4,7 +4,9 @@ from . import apis
 
 app_name = 'cars'
 urlpatterns = [
+    path('', apis.CarListAPIView.as_view(), name='main_car'),
     path('new/', apis.CarAPIView.as_view(), name='create_car'),
     path('<int:car_id>/', apis.CarAPIView.as_view(), name='detail_car'),
     path('<int:car_id>/approval/', apis.CarAPIView.as_view(), name='modify_car_status'),
+
 ]
