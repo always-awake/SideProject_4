bf45d0ace997a74890bc176dd3244a8740f51785
leemirim <leemirim9654@gmail.com>
2019-04-09 00:56:47 +0900

    Brand, Kind, Model 모델 Admin 등록

--- app/cars/admin.py
+++ app/cars/admin.py
@@ -2,6 +2,35 @@ from django.contrib import admin
 from . import models
 
 
+@admin.register(models.Brand)
+class BrandAdmin(admin.ModelAdmin):
+    
+    list_display = (
+        'id',
+        'name',
+        'car_count',
+    )
+
+
+@admin.register(models.Kind)
+class KindAdmin(admin.ModelAdmin):
+    
+    list_display = (
+        'id',
+        'name',
+        'car_count',
+    )
+
+
+@admin.register(models.Model)
+class ModelAdmin(admin.ModelAdmin):
+    
+    list_display = (
+        'id',
+        'name',
+        'car_count',
+    )
+
 @admin.register(models.Car)
 class CarAdmin(admin.ModelAdmin):
 
@@ -11,16 +40,12 @@ class CarAdmin(admin.ModelAdmin):
         'auction_start_time',
         'auction_end_time',
         'owner',
-        'brand',
-        'kind',
         'model',
     )
     
     list_filter = (
         'status',
         'owner',
-        'brand',
-        'kind',
         'model',
     )
 
