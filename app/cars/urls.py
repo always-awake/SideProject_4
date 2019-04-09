b128c8f78aacaddfd29536a87a98d8d2e1b2c8e7
leemirim <leemirim9654@gmail.com>
2019-04-09 16:10:37 +0900

    필터 기능을 제공하는 SearchView url 생성

--- app/cars/urls.py
+++ app/cars/urls.py
@@ -4,9 +4,9 @@ from . import apis
 
 app_name = 'cars'
 urlpatterns = [
-    path('', apis.CarListAPIView.as_view(), name='car_list'),
-    path('new/', apis.CarCreateAPIView.as_view(), name='car_new'),
-    path('<int:car_id>/', apis.CarDetailAPIView.as_view(), name='car_detail'),
-    path('<int:car_id>/approval/', apis.CarCreateAPIView.as_view(), name='car_modify_status'),
-
+    path('', apis.CarListView.as_view(), name='car_list'),
+    path('new/', apis.CarCreateView.as_view(), name='car_new'),
+    path('<int:car_id>/', apis.CarDetailView.as_view(), name='car_detail'),
+    path('<int:car_id>/approval/', apis.CarCreateView.as_view(), name='car_modify_status'),
+    path('search/', apis.SearchView.as_view(), name='search'),
 ]
